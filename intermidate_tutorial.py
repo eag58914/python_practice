18b871b903493bbad11378143cd637fcc9456791
Elco Garcia <garciaelco18@gmail.com>
2019-10-18 16:34:37 -0500

    created simple argparse for cli

--- intermidate_tutorial.py
+++ intermidate_tutorial.py
@@ -22,23 +22,36 @@ import sys
 # print('{} bought{} apples today!'.format(who, how_many))
 
 # argparse for CLI (part) 3
+# convert to argparse for cli
 
 def main():
-    parser = argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--x', type=float, default=1.0,
+                        help="What is the first number?"
+                        )
+    parser.add_argument('--y', type=float, default=1.0,
+                        help="What is the second number?"
+                        )
+    parser.add_argument('--operation', type=str, default='add',
+                        help="What operation? (add,sub,mul, or div)"
+                        )
+    args = parser.parse_args()
+    sys.stdout.write(str(calc(args)))
 
 
-def calc(x, y, operation):
-    if operation == 'add':
-        return x + y
-    elif operation == 'sub':
-        return x - y
-    elif operation == 'mul':
-        return x * y
-    elif operation == 'div':
-        return x / y
+def calc(args):
+    if args.operation == 'add':
+        return args.x + args.y
+    elif args.operation == 'sub':
+        return args.x - args.y
+    elif args.operation == 'mul':
+        return args.x * args.y
+    elif args.operation == 'div':
+        return args.x / args.y
 
 
-operation = calc(7, 3, 'div')
-print(operation)
+if __name__ == '__main__':
+    main()
 
-# convert to argparse for cli
+
+# so much  fun
